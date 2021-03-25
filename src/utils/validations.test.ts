2080cf0d16b5fb7869ebde04e6ad20678f24d698
Matt Daley <mdaleyhk@gmail.com>
2021-03-25 16:22:28 +0000

    add basic email validation for building out form

---
+++ src/utils/validations.test.ts
@@ -0,0 +1,33 @@
+import * as validation from '../utils/validation';
+
+describe('#validations', () => {
+  describe('#isEmail', () => {
+    test('it returns false if empty string is passed', () => {
+      const email = '';
+      const result = validation.isEmail(email);
+      expect(result).toBe(false);
+    });
+    test('it returns true for valid emails', () => {
+      let email = 'me@me.com';
+      let result = validation.isEmail(email);
+      expect(result).toBe(true);
+
+      email = 'a@hotmail.co.uk';
+      result = validation.isEmail(email);
+      expect(result).toBe(true);
+
+      email = 'a@hotmail.co.nz';
+      result = validation.isEmail(email);
+      expect(result).toBe(true);
+    });
+    test('it returns false for invalid emails', () => {
+      let email = 'me@me';
+      let result = validation.isEmail(email);
+      expect(result).toBe(false);
+
+      email = 'a@hotmailcom';
+      result = validation.isEmail(email);
+      expect(result).toBe(false);
+    });
+  });
+});
---
+++ src/utils/validations.ts
@@ -0,0 +1,4 @@
+export const isEmail = (email: string): boolean => {
+  const emailRegex = /^\S+@\S+\.\S+$/;
+  return emailRegex.test(email);
+};
