639cb61ef3515786dcbeb035030c81d42e0783a7
Matt Daley <mdaleyhk@gmail.com>
2021-06-23 19:38:06 +0100

    adds Redux scaffold code and adds types

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 import './App.css';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import FilmList from './components/FilmList/FilmList';
+import { store } from './store';
 
 function App() {
   return (
-    <div className='container mx-auto py-20 px-10'>
-      <Router>
-        <Switch>
-          <Route path='/register'>
-            <RegisterPage />
-          </Route>
-          <Route path='/films'>
-            <FilmList />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className='container mx-auto py-20 px-10'>
+        <Router>
+          <Switch>
+            <Route path='/register'>
+              <RegisterPage />
+            </Route>
+            <Route path='/films'>
+              <FilmList />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
