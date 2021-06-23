90592e4a993bef6b34579e401ecead269749a0b6
Matt Daley <mdaleyhk@gmail.com>
2021-06-23 20:09:43 +0100

    adds root reducer and outline for film reducer

---
+++ src/reducers/filmsReducer.ts
@@ -0,0 +1,12 @@
+import { Film } from '../ts/common/pageStates';
+
+const filmsReducer = (state = [], action: { type: string; films: Film[] }) => {
+  switch (action.type) {
+    case 'ADD_FILMS':
+      return [...state, action.films];
+    default:
+      return state;
+  }
+};
+
+export default filmsReducer;
